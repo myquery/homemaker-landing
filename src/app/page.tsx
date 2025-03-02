8094a1824439d4a21d9545cd1deed3bec86cfff2
Ibe Arua <stanarua@aol.com>
2025-03-03 00:29:24 +0100

    landing-page-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Homemaker App, Making shopping rewarding",
+  description: "HomeMaker transforms household spending into valuable rewards for buyers while maximizing profits for sellers,
   // other metadata
 };
 
