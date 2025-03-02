edebd0704229e4ffa3f38b386901a66260bb0cae
Ibe Arua <stanarua@aol.com>
2025-03-02 21:15:09 +0100

    Fix ESLint error and update browserslist

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -13,11 +13,11 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                HomeMaker App &ndash; Making Every Transaction Rewarding for Buyers and Profitable for Sellers
+                Making Every Transaction Rewarding for Buyers and Profitable for Sellers
 
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                HomeMaker App transforms household spending into valuable rewards for buyers while maximizing profits for sellers. Whether you&apos;re purchasing essentials or selling your products, our platform ensures a seamless and beneficial experience for both parties.
+                HomeMaker transforms household spending into valuable rewards for buyers while maximizing profits for sellers. Whether you are purchasing essentials or selling your products, our platform ensures a seamless and beneficial experience for both parties.
 
                </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
